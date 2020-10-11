2f516fc196b9ff27eb862948b87f74dc2199d45a
Thilina-Pathirage <thilinapathirage@gmail.com>
2020-10-12 00:16:40 +0530

    Update

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void binarySearch(int size,int arr[], int sk){
+    
+    // nishu
+    
+    
+    double start=clock();
+    
+    
+    // avishka
+    
+    printf("Position is : %d \n",position);
+    double end=clock();
+    double timetakes=(double)(end-start);
+    printf("Takes time is : %lf \n",timetakes);
+}
+
+int main(){
+    
+    // suraji
+    
+}
+
